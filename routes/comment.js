eca38106ebf0b8fc4c37a56ed40701d88c1133d6
eva <972080962@qq.com>
2021-05-17 17:51:55 +0800

    no message

--- routes/comment.js
+++ routes/comment.js
@@ -111,7 +111,7 @@ body:
 router.post('/delete', (req, res) => {
   const body = req.body;
   var c_id = body.c_id;
-  var sql = "delete * from comment where c_id = '"+c_id+"' ";
+  var sql = "delete * from `Comment` where c_id = '"+c_id+"' ";
     db.query(sql, [], function (results, fields) {
       console.log(results);
       res.send({
